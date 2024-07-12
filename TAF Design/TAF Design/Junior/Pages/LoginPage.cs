6ea5d057da8878749a997ca033644e39a55bbafb
Vladyslav Hanoshenko <Vladyslav_Hanoshenko1@epam.com>
2024-07-12 14:40:14 +0200

    Added example of explicit waits for Junior

--- TAF Design/TAF Design/Junior/Pages/LoginPage.cs
+++ TAF Design/TAF Design/Junior/Pages/LoginPage.cs
@@ -1,19 +1,23 @@
 ﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
 
 namespace Junior.Pages
 {
     public class LoginPage
     {
         private readonly IWebDriver driver;
+        private readonly WebDriverWait wait;
 
         public LoginPage(IWebDriver driver)
         {
             this.driver = driver;
+            wait = new WebDriverWait(this.driver, TimeSpan.FromSeconds(5));
         }
 
-        public IWebElement UserNameField => driver.FindElement(By.XPath("//input[@name='username']"));
-        public IWebElement PasswordField => driver.FindElement(By.XPath("//input[@name='password']"));
-        public IWebElement LoginButton => driver.FindElement(By.XPath("//button[@type='submit']"));
+        public IWebElement UserNameField => wait.Until(ExpectedConditions.ElementExists(By.XPath("//input[@name='username']")));
+        public IWebElement PasswordField => wait.Until(ExpectedConditions.ElementExists(By.XPath("//input[@name='password']")));
+        public IWebElement LoginButton => wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//button[@type='submit']")));
 
         public void LoginIntoTheApplication(string userName, string password)
         {
